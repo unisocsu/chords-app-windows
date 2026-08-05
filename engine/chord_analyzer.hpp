5eadd74a9861b403fa2d060bba1c099f081db616
unisocsu <rmk0557603971@outlook.com>
2026-08-05 16:18:52 +0300

    Create chord_analyzer.hpp

---
+++ engine/chord_analyzer.hpp
@@ -0,0 +1,14 @@
+#ifndef CHORDSTUDIO_CHORD_ANALYZER_HPP
+#define CHORDSTUDIO_CHORD_ANALYZER_HPP
+
+namespace chordstudio {
+
+class ChordAnalyzer {
+public:
+    ChordAnalyzer() = default;
+    ~ChordAnalyzer() = default;
+};
+
+} // namespace chordstudio
+
+#endif
