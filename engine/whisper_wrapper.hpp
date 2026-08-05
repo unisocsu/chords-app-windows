d0fb1d72159cf2dd8159f41068a00596d7beef5e
unisocsu <rmk0557603971@outlook.com>
2026-08-05 16:20:51 +0300

    Create whisper_wrapper.hpp

---
+++ engine/whisper_wrapper.hpp
@@ -0,0 +1,14 @@
+#ifndef CHORDSTUDIO_WHISPER_WRAPPER_HPP
+#define CHORDSTUDIO_WHISPER_WRAPPER_HPP
+
+namespace chordstudio {
+
+class WhisperWrapper {
+public:
+    WhisperWrapper() = default;
+    ~WhisperWrapper() = default;
+};
+
+} // namespace chordstudio
+
+#endif
