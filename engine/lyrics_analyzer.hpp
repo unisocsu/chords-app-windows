97c4e6e3f4a2918441bfa9b4f94cbb4e7139afd9
unisocsu <rmk0557603971@outlook.com>
2026-08-05 16:20:12 +0300

    Create lyrics_analyzer.hpp

---
+++ engine/lyrics_analyzer.hpp
@@ -0,0 +1,14 @@
+#ifndef CHORDSTUDIO_LYRICS_ANALYZER_HPP
+#define CHORDSTUDIO_LYRICS_ANALYZER_HPP
+
+namespace chordstudio {
+
+class LyricsAnalyzer {
+public:
+    LyricsAnalyzer() = default;
+    ~LyricsAnalyzer() = default;
+};
+
+} // namespace chordstudio
+
+#endif
