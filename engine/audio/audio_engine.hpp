f9a0658e03c3f9f167301b9bc8049453b63b72ed
unisocsu <rmk0557603971@outlook.com>
2026-08-05 16:52:03 +0300

    Create audio_engine.hpp

---
+++ engine/audio/audio_engine.hpp
@@ -0,0 +1,23 @@
+#ifndef CHORDSTUDIO_AUDIO_ENGINE_HPP
+#define CHORDSTUDIO_AUDIO_ENGINE_HPP
+
+#include <string>
+
+namespace chordstudio {
+
+class AudioEngine {
+public:
+    AudioEngine();
+    ~AudioEngine();
+
+    bool initialize();
+    void shutdown();
+    bool loadFile(const std::string& filePath);
+
+private:
+    bool m_isInitialized{false};
+};
+
+} // namespace chordstudio
+
+#endif
